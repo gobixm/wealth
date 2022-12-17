a5df2a3315ca2d0651d1af99798d1d48f4b682c0
Kirill Ivanov <gobixm@gmail.com>
2022-12-17 16:03:03 +0300

    feat: securities list

--- Wealth/Wealth.Frontend/src/lib/SecuritiesSettings.svelte
+++ Wealth/Wealth.Frontend/src/lib/SecuritiesSettings.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
   import Button, { Label } from "@smui/button";
+  import DataTable, {
+    Body,
+    Cell,
+    Head,
+    Pagination,
+    Row,
+  } from "@smui/data-table";
+  import IconButton from "@smui/icon-button";
+  import { onMount } from "svelte";
   import { apiUrl } from "./api";
 
   interface SecuritySyncProgress {
@@ -8,8 +17,24 @@
     count: number;
   }
 
+  interface Security {
+    id: string;
+    name: string;
+    modified: Date;
+    deleted: boolean;
+    term: number;
+  }
+
   let progress: SecuritySyncProgress;
   let progressTimer: NodeJS.Timer;
+
+  let currentPage = 0;
+  const pageSize = 25;
+  let securities: Security[] = [];
+
+  onMount(async () => {
+    await getSecurities();
+  });
 
   const sync = async () => {
     const response = await fetch(`${apiUrl}/secs/sync`, { method: "POST" });
@@ -28,10 +53,76 @@
       clearInterval(progressTimer);
     }
   };
+
+  const getSecurities = async () => {
+    const respose = await fetch(
+      `${apiUrl}/secs?offset=${currentPage * pageSize}&limit=${pageSize}`
+    );
+
+    securities = await respose.json();
+  };
+
+  const movePage = async (offset: number) => {
+    currentPage += offset;
+    if (currentPage < 0) currentPage = 0;
+    await getSecurities();
+  };
 </script>
 
-<Button on:click={sync}><Label>Sync All Securities</Label></Button>
+<div class="sec-admin">
+  <Button on:click={sync}><Label>Sync All Securities</Label></Button>
+  {#if progress && !progress.completed}
+    <div>Progress: {progress.count}, completed: {progress.completed}</div>
+  {/if}
 
-{#if progress && !progress.completed}
-  <div>Progress: {progress.count}, completed: {progress.completed}</div>
-{/if}
+  <DataTable class="sec-list"
+    ><Head>
+      <Row>
+        <Cell>Name</Cell>
+        <Cell>ID</Cell>
+        <Cell>Deleted</Cell>
+        <Cell>Modified</Cell>
+        <Cell>Term</Cell>
+      </Row>
+    </Head>
+    <Body>
+      {#each securities as security (security.id)}
+        <Row>
+          <Cell>{security.name}</Cell>
+          <Cell>{security.id}</Cell>
+          <Cell>{security.deleted ? "deleted" : ""}</Cell>
+          <Cell>{security.modified}</Cell>
+          <Cell>{security.term}</Cell>
+        </Row>
+      {/each}
+    </Body>
+
+    <Pagination slot="paginate">
+      <IconButton
+        class="material-icons"
+        action="prev-page"
+        title="Prev page"
+        on:click={async () => await movePage(-1)}
+        disabled={currentPage === 0}>chevron_left</IconButton
+      >
+      <IconButton
+        class="material-icons"
+        action="next-page"
+        title="Next page"
+        on:click={async () => await movePage(1)}>chevron_right</IconButton
+      >
+    </Pagination>
+  </DataTable>
+</div>
+
+<style lang="scss">
+  .sec-list {
+    display: flex;
+    flex-flow: column nowrap;
+  }
+
+  .sec-admin {
+    display: flex;
+    flex-flow: column nowrap;
+  }
+</style>
